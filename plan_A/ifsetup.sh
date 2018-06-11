3c34b4dbc4778cd4164ddb98ae32d7fe32e08f37
eliasthunderdog <renzhenghang@hotmail.com>
2018-06-11 14:34:29 +0800

    fix faults with eth0

--- plan_A/ifsetup.sh
+++ plan_A/ifsetup.sh
@@ -2,7 +2,6 @@
 echo "setting the interface"
 echo "please run this script as root user !"
 
-ip addr flush eth0
 ip tuntap add tap0 mode tap
 ip addr add 10.0.120.100/24 dev tap0
 ip link set dev tap0 up
